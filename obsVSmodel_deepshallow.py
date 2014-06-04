1f132dc0c9c7194e21f40724aa876c22923faf85
jian.cui <acui145@gmail.com>
2014-06-04 15:31:32 -0400

    fix nan data of tempModShallow

--- obsVSmodel_deepshallow.py
+++ obsVSmodel_deepshallow.py
@@ -10,11 +10,12 @@ def str2float(arg):
     ret = []
     for i in arg:
         i = i.split(',')
-        b = []
+        b = np.array([])
         for j in i:
             j = float(j)
-            b.append(j)
+            b = np.append(b, j)
         ret.append(b)
+    ret = np.array(ret)
     return ret
 FONTSIZE = 25
 ctd = pd.read_csv('ctd_extract_good.csv')
@@ -35,16 +36,16 @@ tempMod = tempObj.watertemp(ctdLon.values, ctdLat.values, ctdDepth.values, ctdTi
 # dic = {'tempMod': tempMod, 'tempObs': ctdTemp, depth: ctdDepth}
 # tempObs = pd.DataFrame(dic, index=tf_index)
 
-tempObsShallow, tempObsShallow = [], []
-tempModDeep, tempModDeep = [], []
-for i in range(len(ctdTime.values))
+tempObsDeep, tempObsShallow = [], []
+tempModDeep, tempModShallow = [], []
+for i in range(len(ctdTime.values)):
     for j in range(len(ctdDepth.values[i])):
         print i, j
         if ctdDepth.values[i][j] > 50.0:
-            tempObsDeep.append(ctdTemp[i][j])
+            tempObsDeep.append(ctdTemp.values[i][j])
             tempModDeep.append(tempMod[i][j])
         else:
-            tempObsShallow.append(ctdTemp[i][j])
+            tempObsShallow.append(ctdTemp.values[i][j])
             tempModShallow.append(tempMod[i][j])
 
 x = np.arange(0, 30, 0.01)
@@ -52,26 +53,30 @@ fig1 = plt.figure()
 ax1 = fig1.add_subplot(111)
 ax1.scatter(tempObsDeep, tempModDeep, s=50, c='b')
 ax1.plot(x, x, 'r-')
-fit1 = np.ployfit(tempObsDeep, tempModDeep, 1)
-fit_fn1 = np.ploy1d(fit1)
+fit1 = np.polyfit(tempObsDeep, tempModDeep, 1)
+fit_fn1 = np.poly1d(fit1)
 ax1.plot(tempObsDeep, fit_fn1(tempObsDeep), 'y--')
 gradient1, intercept1, r_value1, p_value1, std_err1\
     = stats.linregress(tempObsDeep, tempModDeep)
-ax1,set_title('>50m, R-squard: %.4f' % r_value1**2, fontsize=FONTSIZE)
-ax1,set_xlabel('CTD temp', fontsize=FONTSIZE)
+ax1.set_title('>50m, R-squard: %.4f' % r_value1**2, fontsize=FONTSIZE)
+ax1.set_xlabel('CTD temp', fontsize=FONTSIZE)
 ax1.set_ylabel('Model temp', fontsize=FONTSIZE)
 
+
+i = np.where(np.array(tempModShallow)<100) #Some of the data is infinity.
+tempObsShallow1 = np.array(tempObsShallow)[i]
+tempModShallow1 = np.array(tempModShallow)[i]
 fig2 = plt.figure()
 ax2 = fig2.add_subplot(111)
-ax2.scatter(tempObsShallow, tempModShallow)
+ax2.scatter(tempObsShallow1, tempModShallow1)
 ax2.plot(x, x, 'r-')
-fit1 = np.ployfit(tempObsShallow, tempModShallow, 1)
-fit_fn1 = np.ploy1d(fit1)
-ax2.plot(tempObsShallow, fit_fn1(tempObsShallow), 'y--')
-gradient1, intercept1, r_value1, p_value1, std_err1\
-    = stats.linregress(tempObsShallow, tempModShallow)
-ax2,set_title('>50m, R-squard: %.4f' % r_value1**2, fontsize=FONTSIZE)
-ax2,set_xlabel('CTD temp', fontsize=FONTSIZE)
+fit2 = np.polyfit(tempObsShallow1, tempModShallow1, 1)
+fit_fn2 = np.poly1d(fit2)
+ax2.plot(tempObsShallow1, fit_fn2(tempObsShallow1), 'y--')
+gradient2, intercept2, r_value2, p_value2, std_err2\
+    = stats.linregress(tempObsShallow1, tempModShallow1)
+ax2.set_title('<50m, R-squard: %.4f' % r_value2**2, fontsize=FONTSIZE)
+ax2.set_xlabel('CTD temp', fontsize=FONTSIZE)
 ax2.set_ylabel('Model temp', fontsize=FONTSIZE)
 plt.show()
 '''
