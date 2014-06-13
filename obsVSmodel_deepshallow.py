32ea2e25ae3e9c7eda684a97a9b385fd3f62cfb1
jian.cui <acui145@gmail.com>
2014-06-13 16:17:20 -0400

    fix bug, draw dentity map

--- obsVSmodel_deepshallow.py
+++ obsVSmodel_deepshallow.py
@@ -17,6 +17,12 @@ def str2float(arg):
         ret.append(b)
     ret = np.array(ret)
     return ret
+def histogramPoints(x, y, bins):
+    H, xedges, yedges = np.histogram2d(x, y, bins=bins)
+    H = np.rot90(H)
+    H = np.flipud(H)
+    Hmasked = np.ma.masked_where(H==0, H)
+    return xedges, yedges, Hmasked
 FONTSIZE = 25
 ctd = pd.read_csv('ctd_extract_good.csv')
 tf_index = np.where(ctd['TF'].notnull())[0]
@@ -47,7 +53,8 @@ for i in range(len(ctdTime.values)):
         else:
             tempObsShallow.append(ctdTemp.values[i][j])
             tempModShallow.append(tempMod[i][j])
-
+'''
+#use scatter
 x = np.arange(0, 30, 0.01)
 fig1 = plt.figure()
 ax1 = fig1.add_subplot(111)
@@ -61,7 +68,6 @@ gradient1, intercept1, r_value1, p_value1, std_err1\
 ax1.set_title('>50m, R-squard: %.4f' % r_value1**2, fontsize=FONTSIZE)
 ax1.set_xlabel('CTD temp', fontsize=FONTSIZE)
 ax1.set_ylabel('Model temp', fontsize=FONTSIZE)
-
 
 i = np.where(np.array(tempModShallow)<100) #Some of the data is infinity.
 tempObsShallow1 = np.array(tempObsShallow)[i]
@@ -80,6 +86,7 @@ ax2.set_xlabel('CTD temp', fontsize=FONTSIZE)
 ax2.set_ylabel('Model temp', fontsize=FONTSIZE)
 plt.show()
 '''
+'''
 fig = plt.figure()
 ax = fig.gca(projection='3d')
 x = ctdLon
@@ -90,3 +97,42 @@ ax.contour(x, y, z)
 ax.set_zlim([250,0])
 plt.show()
 '''
+#use histogram2d and pcolormesh
+x = np.arange(0, 30, 0.01)
+fig1 = plt.figure()
+ax1 = fig1.add_subplot(111)
+# ax1.scatter(tempObsDeep, tempModDeep, s=50, c='b')
+x1, y1, Hmasked1 = histogramPoints(tempObsDeep, tempModDeep, 200)
+c1 = ax1.pcolormesh(x1, y1, Hmasked1)
+ax1.plot(x, x, 'r-')
+fit1 = np.polyfit(tempObsDeep, tempModDeep, 1)
+fit_fn1 = np.poly1d(fit1)
+ax1.plot(tempObsDeep, fit_fn1(tempObsDeep), 'y-', linewidth=2)
+gradient1, intercept1, r_value1, p_value1, std_err1\
+    = stats.linregress(tempObsDeep, tempModDeep)
+ax1.set_title('Deep(>50m), R-squard: %.4f' % r_value1**2, fontsize=FONTSIZE)
+ax1.set_xlabel('CTD temp', fontsize=FONTSIZE)
+ax1.set_ylabel('Model temp', fontsize=FONTSIZE)
+cbar = plt.colorbar(c1)
+cbar.ax.set_ylabel('Counts', fontsize=FONTSIZE)
+
+i = np.where(np.array(tempModShallow)<100) #Some of the data is infinity.
+tempObsShallow1 = np.array(tempObsShallow)[i]
+tempModShallow1 = np.array(tempModShallow)[i]
+fig2 = plt.figure()
+ax2 = fig2.add_subplot(111)
+# ax2.scatter(tempObsShallow1, tempModShallow1)
+x2, y2, Hmasked2 = histogramPoints(tempObsShallow1, tempModShallow1, 200)
+c2 = ax2.pcolormesh(x2, y2, Hmasked2)
+ax2.plot(x, x, 'r-')
+fit2 = np.polyfit(tempObsShallow1, tempModShallow1, 1)
+fit_fn2 = np.poly1d(fit2)
+ax2.plot(tempObsShallow1, fit_fn2(tempObsShallow1), 'y-', linewidth=2)
+gradient2, intercept2, r_value2, p_value2, std_err2\
+    = stats.linregress(tempObsShallow1, tempModShallow1)
+ax2.set_title('Shallow(<50m), R-squard: %.4f' % r_value2**2, fontsize=FONTSIZE)
+ax2.set_xlabel('CTD temp', fontsize=FONTSIZE)
+ax2.set_ylabel('Model temp', fontsize=FONTSIZE)
+cbar = plt.colorbar(c2)
+cbar.ax.set_ylabel('Count', fontsize=FONTSIZE)
+plt.show()
